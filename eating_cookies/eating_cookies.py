711d9f0cdd2dabe82e0148d32f94bc7f39d6d5d4
trodriguez89 <trodriguez89@gmail.com>
2020-04-17 07:21:46 -0700

    cookies, and rps

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,25 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n == 0:
+    return 1
+  elif n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  else:
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n - 3)
+
+# def eating_cookies(n, cache=None):
+#     if cache is None:
+#         cache = [0] * (n + 1)
+#     if n <= 1:
+#         cache[n] = 1
+#     elif n == 2:
+#         cache[n] = 2
+#     elif cache[n] == 0:
+#         cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+#     return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
